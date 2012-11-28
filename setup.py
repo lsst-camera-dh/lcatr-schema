548eb81d9d4be5b0810881c5aaf796853f1e31df
Brett Viren <brett.viren@gmail.com>
2012-11-28 18:10:38 -0500

    First commit of setup.py installation file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from glob import glob
+from distutils.core import setup
+setup(name='schema',
+      version='0',
+      url='https://git.racf.bnl.gov/astro/cgit/lcatr/schema.git',
+      author='Brett Viren',
+      author_email='bv@bnl.gov',
+      package_dir = {'': 'python'},
+      packages = ['lcatr.schema'],
+      data_files = [('examples',glob('schemas/*.schema')),
+                    ('doc',['doc/schema.org'])]
+      )
